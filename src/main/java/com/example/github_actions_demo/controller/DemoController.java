8d913a60aee86c42458fab6dd0c1111ad136cbf9
Rounak Das <rounak.das@nagarro.com>
2026-02-13 11:14:38 +0530

    first commit

---
+++ src/main/java/com/example/github_actions_demo/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.github_actions_demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to Github actions Demo!!";
+    }
+}
